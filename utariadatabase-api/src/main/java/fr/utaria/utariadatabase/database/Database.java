00a3859940298e3438f8a08dbdb36415dafac920
utarwyn <maxime.malgorn@laposte.net>
2017-12-29 16:26:39 +0100

    Fix javadoc + Premières classes API

---
+++ utariadatabase-api/src/main/java/fr/utaria/utariadatabase/database/Database.java
@@ -0,0 +1,7 @@
+package fr.utaria.utariadatabase.database;
+
+public abstract class Database {
+
+
+
+}
---
+++ utariadatabase-api/src/main/java/fr/utaria/utariadatabase/database/DatabaseAccessor.java
@@ -0,0 +1,16 @@
+package fr.utaria.utariadatabase.database;
+
+public abstract class DatabaseAccessor {
+
+	public DatabaseAccessor(String databaseName) {
+	}
+
+	String getDBName() {
+		return null;
+	}
+
+	public Database getDB() {
+		return null;
+	}
+
+}
